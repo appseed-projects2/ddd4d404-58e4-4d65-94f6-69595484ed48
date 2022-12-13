b6f55a2f00583e447a3b255f12bd31ef140582d7
AppSeed Generated Projects <108474434+appseed-projects2@users.noreply.github.com>
2022-12-13 03:41:25 +0200

    initial commit

---
+++ react-ui/src/layouts/sections/navigation/navbars/components/NavbarDark/code.js
@@ -0,0 +1,32 @@
+const navbarDarkCode = `// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+
+// Routes
+import routes from "routes";
+
+function NavbarDark() {
+  return (
+    <MKBox variant="gradient" bgColor="dark" shadow="sm" py={0.25}>
+      <DefaultNavbar
+        routes={routes}
+        action={{
+          type: "external",
+          route: "https://appseed.us/product/material-kit/api-server-nodejs/react/",
+          label: "download",
+          color: "info",
+        }}
+        transparent
+        relative
+        light
+        center
+      />
+    </MKBox>
+  );
+}
+
+export default NavbarDark;`;
+
+export default navbarDarkCode;
